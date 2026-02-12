ae2c05468412f176f75a1edf615d2eb1318ba95b
Carlos Ramirez <crbarrio@hotmail.com>
2026-02-12 16:33:24 +0100

    feat: enhance pagination logic and display for general results

--- src/js/ui.js
+++ src/js/ui.js
@@ -51,15 +51,81 @@ export function displayGeneralResults(items) {
     const list = Array.isArray(items) ? items : [items];
     let output = '';
 
+
     list.forEach(item => {
+        const page = Math.ceil((list.indexOf(item) + 1) / itemsPerPage);
         output += `
-            <div class="text-start border rounded-sm p-2 shadow-sm bg-white">
+            <div class="generalItems text-start border rounded-sm p-2 shadow-sm bg-white hidden" data-page="${page}">
                 <pre class="whitespace-pre-wrap text-xs">${escapeHtml(formatAny(item))}</pre>
             </div>`;
     });
 
     resultsContainer.innerHTML = output;
+
+    setupGeneralPagination(list.length)
 }
+
+function setupGeneralPagination(totalItems) {
+    paginationContainer.innerHTML = '';
+    const pages= Math.ceil(totalItems / itemsPerPage);
+
+    if (pages <= 1) return
+
+    let output = ``;
+
+    for (let index = 1; index <= pages; index++) {
+
+        let activeAttr = '';
+        let buttonClass = 'pageGeneralButton font-medium leading-5 text-sm px-3 py-2';
+
+        if (index === currentPage) {
+            buttonClass += ' bg-gray-300';
+            activeAttr = ' aria-current="page" disabled';
+        } else {
+            buttonClass += ' hover:bg-gray-200 focus:ring-1 focus:ring-gray-300';
+        }
+
+        output += `<button data-page="${index}" class="${buttonClass}"${activeAttr}>${index}</button>`
+        
+    }
+
+    paginationContainer.innerHTML = output
+
+    const pageButtons = document.querySelectorAll('.pageGeneralButton')
+    const generalItems = document.querySelectorAll('.generalItems')
+
+    
+    generalItems.forEach(item => item.classList.add('hidden'))
+    generalItems.forEach(item => {
+        if (Number(item.dataset.page) === currentPage) {
+            item.classList.remove('hidden')
+        }
+    })
+
+    
+    pageButtons.forEach(btn =>
+        btn.addEventListener('click', (e) => {
+            setPage(Number(e.currentTarget.dataset.page))
+            pageButtons.forEach(btn => {
+                btn.classList.remove('bg-gray-300')
+                btn.removeAttribute('aria-current')
+                btn.removeAttribute('disabled')
+            })
+            e.currentTarget.classList.add('bg-gray-300')
+            e.currentTarget.setAttribute('aria-current', 'page')
+            e.currentTarget.setAttribute('disabled', 'disabled')
+
+            generalItems.forEach(item => item.classList.add('hidden'))
+            generalItems.forEach(item => {
+                if (Number(item.dataset.page) === currentPage) {
+                    item.classList.remove('hidden') 
+                }
+            })
+        })
+    )
+
+}
+
 
 
 
